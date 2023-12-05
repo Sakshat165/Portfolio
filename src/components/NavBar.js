7734d3a68e586cda15c0eb416a90854b84582389
Sakshat165 <sakshat.worksite@gmail.com>
2023-12-05 09:14:15 +0530

    routing nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,14 +11,14 @@ const NavBar = () => {
       <nav className="navbar navbar-expand-lg colornav">
   <div className="container-fluid">
     <span className="navbar-brand"><img src={img} alt="" className ="image--cover" /></span>
-    <Link to='/'><button className="navbar-toggler" type="button">
+    <Link to='/Portfolio'><button className="navbar-toggler" type="button">
       <span className="fa-solid fa-house fa-2x"></span>
     </button>
     </Link>
     <div className="container name d-flex justify-content-center">
         Sakshat Shetty
     </div>
-    <Link to='/'>
+    <Link to='/Portfolio'>
     <div className="home">
         <button className='home1'><i className="fa-solid fa-house fa-2x" ></i></button>
     </div>
